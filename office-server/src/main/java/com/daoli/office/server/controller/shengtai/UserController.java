d568b52a5c4cf8fadecfa17a525944fe1ebb7589
语露 <lining.wln@alibaba-inc.com>
2019-10-04 09:05:15 +0800

    user add && verify

--- office-server/src/main/java/com/daoli/office/server/controller/shengtai/UserController.java
+++ office-server/src/main/java/com/daoli/office/server/controller/shengtai/UserController.java
@@ -2,11 +2,40 @@ package com.daoli.office.server.controller.shengtai;
 
 import com.daoli.office.vo.JsonResponse;
 import com.daoli.office.vo.sheng.tai.UserVo;
+import com.daoli.sheng.tai.HttpUtils.PostTool;
 import com.daoli.sheng.tai.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+//import org.apache.tomcat.util.http.fileupload.FileItem;
+//import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+//import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import  org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Created by wanglining on 2019/10/3.
@@ -27,10 +56,48 @@ public class UserController {
     }
 
     @ResponseBody
+    @ApiOperation(value = "pid")
+    @RequestMapping(value = "/query_user_has_face_info", method = RequestMethod.GET)
+    public JsonResponse registerUser(Integer pid){
+        return new JsonResponse(userService.isUserHasFaceInfo(pid));
+    }
+
+    @ResponseBody
+    @ApiOperation(value = "用户人脸上传接口，pid")
+    @RequestMapping(value = "/user_face_upload", method = RequestMethod.POST)
+    public JsonResponse uploadUserFace( HttpServletRequest request, HttpServletResponse response){
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("'Access-Control-Allow-Headers","x-requested-with");
+        response.setHeader("Access-Control-Allow-Methods","POST, GET, PUT, DELETE");
+
+        String pid = request.getParameter("pid");
+        String userName = request.getParameter("user_name");
+        String writePicToDisk = request.getParameter("write_pic_to_disk");
+        MultipartHttpServletRequest mul = (MultipartHttpServletRequest)request;
+        MultipartFile multipartFile = mul.getFile("image_content");
+
+
+        if(StringUtils.isNotEmpty(pid) == false  || StringUtils.isNotEmpty(userName) == false
+                || multipartFile == null){
+            return new JsonResponse("pid/user_name is empty; or camera has problem.");
+        }
+
+        return new JsonResponse(userService.addUserFace(pid, userName, multipartFile));
+    }
+
+    @ResponseBody
     @ApiOperation(value = "用户登录, login_name/login_password ")
     @RequestMapping(value = "/user_login", method = RequestMethod.POST)
-    public JsonResponse verifyUser(@RequestBody UserVo userVo){
-        return new JsonResponse(userService.verifyUser(userVo));
+    public JsonResponse verifyUser(HttpServletRequest request,HttpServletResponse response){
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("'Access-Control-Allow-Headers","x-requested-with");
+        response.setHeader("Access-Control-Allow-Methods","POST, GET, PUT, DELETE");
+
+        MultipartHttpServletRequest mul = (MultipartHttpServletRequest)request;
+        MultipartFile multipartFile = mul.getFile("image_content");
+
+        String verifyFaceResp = PostTool.postImage("http://localhost:8082/verify_video_picture",new HashMap<>(), multipartFile);
+        return new JsonResponse(verifyFaceResp);
     }
 
 }
--- sheng-tai/src/main/java/com/daoli/sheng/tai/HttpUtils/PostTool.java
+++ sheng-tai/src/main/java/com/daoli/sheng/tai/HttpUtils/PostTool.java
@@ -1,13 +1,16 @@
 package com.daoli.sheng.tai.HttpUtils;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by wanglining on 2019/10/3.
@@ -21,7 +24,9 @@ public class PostTool {
         //开始访问
         StringBuilder postData = new StringBuilder();
         for (Map.Entry<String,Object> param : params.entrySet()) {
-            if (postData.length() != 0) postData.append('&');
+            if (postData.length() != 0) {
+                postData.append('&');
+            }
             postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
             postData.append('=');
             postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
@@ -32,6 +37,8 @@ public class PostTool {
         conn.setRequestMethod("POST");
         conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
         conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+
+
         conn.setDoOutput(true);
         conn.getOutputStream().write(postDataBytes);
 
@@ -52,6 +59,112 @@ public class PostTool {
 
         }
         return response;
+    }
+
+    public static String postImage(String arg_url, Map<String,Object> param, MultipartFile multipartFile ){
+        String result = null;
+        String BOUNDARY = UUID.randomUUID().toString();  //边界标识   随机生成
+        String PREFIX = "--", LINE_END = "\r\n";
+        String CONTENT_TYPE = "multipart/form-data";   //内容类型
+        // 显示进度框
+        //      showProgressDialog();
+        try {
+            URL url = new URL(arg_url);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            //conn.setReadTimeout(TIME_OUT);
+            //conn.setConnectTimeout(TIME_OUT);
+            conn.setDoInput(true);  //允许输入流
+            conn.setDoOutput(true); //允许输出流
+            conn.setUseCaches(false);  //不允许使用缓存
+            conn.setRequestMethod("POST");  //请求方式
+            conn.setRequestProperty("Charset", "UTF-8");  //设置编码 改
+            conn.setRequestProperty("connection", "keep-alive");
+            conn.setRequestProperty("X-bocang-Authorization", "token"); //token可以是用户登录后的token等等......
+            conn.setRequestProperty("Content-Type", CONTENT_TYPE + ";boundary=" + BOUNDARY);
+            if (multipartFile != null) {
+                /**
+                 * 当文件不为空，把文件包装并且上传
+                 */
+                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+                StringBuffer sb = new StringBuffer();
+
+                String params = "";
+                if (param != null && param.size() > 0) {
+                    Iterator<String> it = param.keySet().iterator();
+                    while (it.hasNext()) {
+                        sb = null;
+                        sb = new StringBuffer();
+                        String key = it.next();
+                        String value = (String) param.get(key);
+                        sb.append(PREFIX).append(BOUNDARY).append(LINE_END);
+                        sb.append("Content-Disposition: form-data; name=\"")
+                                .append(key)
+                                .append("\"")
+                                .append(LINE_END)
+                                .append(LINE_END);
+                        sb.append(value).append(LINE_END);
+                        params = sb.toString();
+                        dos.write(params.getBytes());
+                    }
+                }
+                sb = new StringBuffer();
+                sb.append(PREFIX);
+                sb.append(BOUNDARY);
+                sb.append(LINE_END);
+                /**
+                 * 这里重点注意：
+                 * name里面的值为服务器端需要key   只有这个key 才可以得到对应的文件
+                 * filename是文件的名字，包含后缀名的   比如:abc.png
+                 */
+                sb.append("Content-Disposition: form-data; name=\"")
+                        .append("image_content")
+                        .append("\"")
+                        .append(";filename=\"")
+                        .append("jietu.jpg")
+                        .append("\"\n");
+                sb.append("Content-Type: image/png");
+                sb.append(LINE_END).append(LINE_END);
+                dos.write(sb.toString().getBytes());
+                InputStream is = multipartFile.getInputStream();
+                byte[] bytes = new byte[1024];
+                int len = 0;
+                while ((len = is.read(bytes)) != -1) {
+                    dos.write(bytes, 0, len);
+                }
+
+
+                is.close();
+                //                dos.write(file);
+                dos.write(LINE_END.getBytes());
+                byte[] end_data = (PREFIX + BOUNDARY + PREFIX + LINE_END).getBytes();
+                dos.write(end_data);
+
+                dos.flush();
+                /**
+                 * 获取响应码  200=成功
+                 * 当响应成功，获取响应的流
+                 */
+
+                int res = conn.getResponseCode();
+                System.out.println("res code   ==================" + res);
+                if (res == 200) {
+                    InputStream input = conn.getInputStream();
+                    StringBuffer sb1 = new StringBuffer();
+                    int ss;
+                    while ((ss = input.read()) != -1) {
+                        sb1.append((char) ss);
+                    }
+                    result = sb1.toString();
+                    System.out.println("resp content: " + result);
+                } else {
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     public static void main(String[] args) {
